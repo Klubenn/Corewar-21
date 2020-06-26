c57711bdcd2801addc7f59b87ca084997f35e254
gtristan <gtristan@student.21-school.ru>
2020-06-26 13:31:46 +0300

    merging with Kate's code

---
+++ cmake-build-debug/CMakeFiles/corewar.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/gtristan/CLionProjects/corewar/src/asm.c" "/Users/gtristan/CLionProjects/corewar/cmake-build-debug/CMakeFiles/corewar.dir/src/asm.c.o"
+  "/Users/gtristan/CLionProjects/corewar/src/checks.c" "/Users/gtristan/CLionProjects/corewar/cmake-build-debug/CMakeFiles/corewar.dir/src/checks.c.o"
+  "/Users/gtristan/CLionProjects/corewar/src/labels.c" "/Users/gtristan/CLionProjects/corewar/cmake-build-debug/CMakeFiles/corewar.dir/src/labels.c.o"
+  "/Users/gtristan/CLionProjects/corewar/src/op.c" "/Users/gtristan/CLionProjects/corewar/cmake-build-debug/CMakeFiles/corewar.dir/src/op.c.o"
+  "/Users/gtristan/CLionProjects/corewar/src/to_bytecode.c" "/Users/gtristan/CLionProjects/corewar/cmake-build-debug/CMakeFiles/corewar.dir/src/to_bytecode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././libft"
+  ".././includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
